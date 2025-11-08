645b37fca57a4989b77ca8a722d9c6da80e8f573
hailex798 <kshitij.singh.bisht.174sis@gmail.com>
2025-11-09 00:13:36 +0530

    removed dist file

--- server/dist/utils/slug.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toSlug = void 0;
-const slugify_1 = __importDefault(require("slugify"));
-const toSlug = (s) => (0, slugify_1.default)(s, { lower: true, strict: true, trim: true });
-exports.toSlug = toSlug;
